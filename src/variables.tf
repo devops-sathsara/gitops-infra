cedbd717923aa26453ecc701079517199c5401c1
devops-sathsara <127945249+devops-sathsara@users.noreply.github.com>
2023-03-20 16:38:53 +0000

    Moved variables

--- src/variables.tf
+++ src/variables.tf
@@ -7,3 +7,18 @@ variable "region" {
   description = "GCP region name"
   type        = string
 }
+
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default     = 1
+  description = "number of gke nodes"
+}
