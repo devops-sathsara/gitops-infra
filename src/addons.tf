ab52ee0f3868efadee8cbd4a9038f911611996ca
devops-sathsara <127945249+devops-sathsara@users.noreply.github.com>
2023-03-20 18:05:36 +0000

    Added kubectl_manifest resource

--- src/addons.tf
+++ src/addons.tf
@@ -66,16 +66,7 @@ data "kubectl_file_documents" "install" {
   content = data.flux_install.main.content
 }
 
-locals {
-  install = [for v in data.kubectl_file_documents.install.documents : {
-    data : yamldecode(v)
-    content : v
-    }
-  ]
-}
-
-resource "kubectl_manifest" "install" {
-  for_each   = { for v in local.install : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-  depends_on = [kubernetes_namespace.flux_system]
-  yaml_body  = each.value
+resource "kubectl_manifest" "test" {
+    for_each  = data.kubectl_file_documents.install.manifests
+    yaml_body = each.value
 }
