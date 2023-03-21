be351fb1fae42f8d4866d3a51f37f6f9038fb92e
devops-sathsara <127945249+devops-sathsara@users.noreply.github.com>
2023-03-21 16:30:25 +0000

    Fixed argocd app dependency

--- src/addons/argocd-app.tf
+++ src/addons/argocd-app.tf
@@ -6,5 +6,5 @@ resource "kubectl_manifest" "my-nginx-app" {
     count               = length(data.kubectl_file_documents.my-nginx-app.documents)
     yaml_body           = element(data.kubectl_file_documents.my-nginx-app.documents, count.index)
     override_namespace  = "argocd"
-    depends_on          = [kubectl_manifest.argocd]
+    depends_on          = [kubectl_manifest.argocd_install]
 }
