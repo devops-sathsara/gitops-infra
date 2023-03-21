775feedf8a25dbae9791ee0dccfb6e446a323506
devops-sathsara <127945249+devops-sathsara@users.noreply.github.com>
2023-03-21 16:10:17 +0000

    Install argocd

--- src/addons/main.tf
+++ src/addons/main.tf
@@ -1,11 +1,4 @@
 
-# # Kubernetes provider
-# # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
-# # It references the variables and resources provisioned in this file. 
-# # We recommend you put this in another file -- so you can have a more modular configuration.
-# # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
-# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
-
 data "google_client_config" "current" { }
 
 data "google_container_cluster" "current" {
@@ -14,15 +7,3 @@ data "google_container_cluster" "current" {
   project  = var.project_id
 }
 
-data "flux_install" "main" {
-  target_path = var.target_path
-}
-
-data "kubectl_file_documents" "install" {
-  content = data.flux_install.main.content
-}
-
-resource "kubectl_manifest" "install" {
-  for_each  = data.kubectl_file_documents.install.manifests
-  yaml_body = each.value
-}
