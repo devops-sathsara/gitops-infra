a481fdbb29424e017ce584e6914a4048c70ee16b
Sathsara Sarathchandra <127945249+devops-sathsara@users.noreply.github.com>
2023-03-24 06:35:32 +0000

    Moved argocd app file

--- src/addons/argocd-app.tf
+++ src/addons/argocd-app.tf
@@ -1,22 +1,11 @@
-data "kubectl_file_documents" "my-nginx-app" {
-    content = file("./manifests/argocd-apps/nginx.yaml")
+data "kubectl_file_documents" "argocd-app" {
+    content = file("./manifests/argocd/argocd-app.yaml")
 }
 
-resource "kubectl_manifest" "my-nginx-app" {
-    count               = length(data.kubectl_file_documents.my-nginx-app.documents)
-    yaml_body           = element(data.kubectl_file_documents.my-nginx-app.documents, count.index)
+resource "kubectl_manifest" "argocd-app" {
+    count               = length(data.kubectl_file_documents.argocd-app.documents)
+    yaml_body           = element(data.kubectl_file_documents.argocd-app.documents, count.index)
     override_namespace  = "argocd"
     depends_on          = [kubectl_manifest.argocd_install]
 }
 
-
-data "kubectl_file_documents" "k8s-addons" {
-    content = file("./manifests/argocd-apps/addons.yaml")
-}
-
-resource "kubectl_manifest" "k8s-addons" {
-    count               = length(data.kubectl_file_documents.k8s-addons.documents)
-    yaml_body           = element(data.kubectl_file_documents.k8s-addons.documents, count.index)
-    override_namespace  = "argocd"
-    depends_on          = [kubectl_manifest.argocd_install]
-}
