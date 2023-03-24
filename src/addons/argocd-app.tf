711d216a8a261348e3fc266d025be8b846e550d7
Sathsara Sarathchandra <127945249+devops-sathsara@users.noreply.github.com>
2023-03-24 05:10:50 +0000

    Added argocd app for k8s addons

--- src/addons/argocd-app.tf
+++ src/addons/argocd-app.tf
@@ -1,10 +1,22 @@
 data "kubectl_file_documents" "my-nginx-app" {
-    content = file("./manifests/argocd/app-nginx.yaml")
+    content = file("./manifests/argocd-apps/nginx.yaml")
 }
 
 resource "kubectl_manifest" "my-nginx-app" {
     count               = length(data.kubectl_file_documents.my-nginx-app.documents)
     yaml_body           = element(data.kubectl_file_documents.my-nginx-app.documents, count.index)
+    override_namespace  = "argocd"
+    depends_on          = [kubectl_manifest.argocd_install]
+}
+
+
+data "kubectl_file_documents" "k8s-addons" {
+    content = file("./manifests/argocd-apps/addons.yaml")
+}
+
+resource "kubectl_manifest" "k8s-addons" {
+    count               = length(data.kubectl_file_documents.k8s-addons.documents)
+    yaml_body           = element(data.kubectl_file_documents.k8s-addons.documents, count.index)
     override_namespace  = "argocd"
     depends_on          = [kubectl_manifest.argocd_install]
 }
