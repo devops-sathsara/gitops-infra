49c165c72517a8eb4088abd7c7297d8b826368c5
Sathsara Sarathchandra <127945249+devops-sathsara@users.noreply.github.com>
2023-03-29 17:00:02 +0000

    Removed project from svc account resource

--- src/cluster/service-account.tf
+++ src/cluster/service-account.tf
@@ -1,10 +1,8 @@
 resource "google_service_account" "artifact_registry" {
   account_id   = "artifact-registry-gke-sa"
   display_name = "Artifact registry access from GKE cluster"
-  project      = var.main_project_id
 }
 
 resource "google_service_account_key" "artifact_registry" {
   service_account_id = google_service_account.artifact_registry.name
-  project      = var.main_project_id
 }
