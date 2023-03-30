b7c352aaf6d00aca2338d867d12cc20f982b54f3
sathsara-cw <sathsara.sarathchandra@cloudways.com>
2023-03-30 15:14:01 +0530

    Removed svc accounts

--- src/cluster/service-account.tf
+++
@@ -1,8 +0,0 @@
-resource "google_service_account" "artifact_registry" {
-  account_id   = "artifact-registry-gke-sa"
-  display_name = "Artifact registry access from GKE cluster"
-}
-
-resource "google_service_account_key" "artifact_registry" {
-  service_account_id = google_service_account.artifact_registry.name
-}
