6efb88eb82ebd47803c0dca3b0c80618fff319f8
Sathsara Sarathchandra <127945249+devops-sathsara@users.noreply.github.com>
2025-03-16 15:10:27 +0100

    Add flux helm chart

--- src/addons/fluxcd.tf
+++ src/addons/fluxcd.tf
@@ -1,26 +1,16 @@
-resource "kubernetes_namespace" "flux_system" {
-  metadata {
-    name = "flux-system"
-  }
+resource "helm_release" "fluxcd" {
+  name       = "fluxcd"
+  repository = "https://fluxcd-community.github.io/helm-charts"
+  chart      = "flux2"
+  namespace  = "flux-system"
+  create_namespace = true
 
-  lifecycle {
-    ignore_changes = [
-      metadata[0].labels,
-    ]
-  }
-}
-
-data "flux_install" "main" {
-  target_path = var.target_path
-}
-
-data "kubectl_file_documents" "flux_install" {
-  content = data.flux_install.main.content
-}
-
-resource "kubectl_manifest" "flux_install" {
-  for_each              = data.kubectl_file_documents.flux_install.manifests
-  yaml_body             = each.value
-  override_namespace    = "flux-system"
-  depends_on            = [kubernetes_namespace.flux_system]
+  values = [
+    <<EOF
+    gitRepository:
+      enabled: true
+      url: "ssh://git@github.com/your-org/your-repo.git"
+      secretRef: flux-system
+    EOF
+  ]
 }
