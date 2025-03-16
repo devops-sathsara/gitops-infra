045c6848eebaef7c1ccfc4eb066ec88a9d7c8314
“sathsara.sarathchandra” <sathsara.sarathchandra@leaseplan.com>
2025-03-16 18:41:44 +0100

    Install flux

--- src/addons/fluxcd.tf
+++ src/addons/fluxcd.tf
@@ -1,16 +1,16 @@
-# resource "helm_release" "fluxcd" {
-#   name       = "fluxcd"
-#   repository = "https://fluxcd-community.github.io/helm-charts"
-#   chart      = "flux2"
-#   namespace  = "flux-system"
-#   create_namespace = true
+resource "helm_release" "fluxcd" {
+  name       = "fluxcd"
+  repository = "https://fluxcd-community.github.io/helm-charts"
+  chart      = "flux2"
+  namespace  = "flux-system"
+  create_namespace = true
 
-#   values = [
-#     <<EOF
-#     gitRepository:
-#       enabled: true
-#       url: "ssh://git@github.com/devops-sathsara/gitops-manifests.git"
-#       secretRef: flux-system
-#     EOF
-#   ]
-# }
+  values = [
+    <<EOF
+    gitRepository:
+      enabled: true
+      url: "ssh://git@github.com/devops-sathsara/gitops-manifests.git"
+      secretRef: flux-system
+    EOF
+  ]
+}
